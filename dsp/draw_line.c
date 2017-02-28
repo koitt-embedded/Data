ef196f5364cd45b61be8575d685b54f413a36291
silenc3502 <gcccompil3r@gmail.com>
2017-03-01 00:17:37 +0900

    test draw line

---
+++ dsp/draw_line.c
@@ -0,0 +1,31 @@
+#include <GL/glut.h>
+
+void display(void)
+{
+	glPointSize(5.0f);      // 정점의 size 조절
+	glClear(GL_COLOR_BUFFER_BIT);
+	glBegin(GL_LINE_STRIP);     // mode 선택
+	glVertex2f(-0.5f, 0.5f);
+	glVertex2f(0.5f, 0.5f);	
+	glVertex2f(-0.5f, -0.5f);
+	glVertex2f(0.5f, -0.5f);
+	glEnd();
+
+	glFlush();
+}
+
+int main(int argc, char* argv[])
+{
+	glutInit(&argc, argv);
+
+
+	glutInitDisplayMode(GLUT_RGBA);
+	glutInitWindowSize(200, 200);
+	glutCreateWindow("Draw Line");
+	
+	glutDisplayFunc(display);
+
+	glutMainLoop();
+
+	return 0;
+}
